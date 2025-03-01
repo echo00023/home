e34c146a7295a43f190cc6fd651ae4ef922de213
Ambition. <102711152+echo00023@users.noreply.github.com>
2025-03-01 17:15:42 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Comments, Avianex, HeartRegular,PictureOutlined} from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Comments, Avianex, HeartRegular,Buffer} from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -73,7 +73,7 @@ const siteIcon = {
   Comments,
   Avianex,
   HeartRegular,
-  PictureOutlined
+  Buffer
 };
 
 // 链接跳转
